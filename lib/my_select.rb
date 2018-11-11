2cd66d5fb0b40b17fd2231850a98ff70a76ac318
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-11 23:58:12 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   selection = []
   while i < my_select.length
-    if yield(collection[i]) do
+    if yield(collection[i])
       selection << collection[i]
     end
     i += 1
